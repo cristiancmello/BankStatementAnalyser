82258eb62f3b1ced3a20f0f85b3f8e5657130666
Cristian Mello <cristianc.mello@gmail.com>
2023-09-26 18:52:07 -0300

    Adding commit hist

--- src/test/java/org/cristiancmello/BankTransactionAnalyserSimpleTest.java
+++ src/test/java/org/cristiancmello/BankTransactionAnalyserSimpleTest.java
@@ -115,6 +115,7 @@ Extracting filename and path as class field from BankTransactionAnalyserSimple
 ...
 
 + more recently
+> f03bba43a5dc298dafc2e9791c02568bc05135ac
 > 00f7afa3e795277fd97b47e4ec40b56009537ce3
 > c52f8c57f3e3a0baedb3d3eed2974a19ef9535ab
 > 26d82d703d363f92bb71d51cab813ed80a66a169
